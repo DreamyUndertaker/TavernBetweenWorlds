8b639d57d718ee08ceb192c03fa52e61e1ffaf25
Вадим Залогин <vadiimreg73@outlook.com>
2021-11-01 01:19:12 +0400

    Почти сделано главное окно.

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -29,13 +29,195 @@ namespace LaunhcerForMinecraft
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.LogoImage = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LoginField = new System.Windows.Forms.TextBox();
+            this.PassField = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.RegistrationLabel = new System.Windows.Forms.Label();
+            this.LostPassLabel = new System.Windows.Forms.Label();
+            this.FAQLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LogoImage
+            // 
+            this.LogoImage.BackColor = System.Drawing.Color.Sienna;
+            this.LogoImage.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("LogoImage.BackgroundImage")));
+            this.LogoImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.LogoImage.Image = ((System.Drawing.Image)(resources.GetObject("LogoImage.Image")));
+            this.LogoImage.Location = new System.Drawing.Point(12, 12);
+            this.LogoImage.Name = "LogoImage";
+            this.LogoImage.Size = new System.Drawing.Size(277, 271);
+            this.LogoImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.LogoImage.TabIndex = 0;
+            this.LogoImage.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(301, 621);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoginField
+            // 
+            this.LoginField.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.LoginField.Location = new System.Drawing.Point(12, 313);
+            this.LoginField.Name = "LoginField";
+            this.LoginField.Size = new System.Drawing.Size(277, 23);
+            this.LoginField.TabIndex = 2;
+            this.LoginField.Enter += new System.EventHandler(this.LoginField_Enter);
+            this.LoginField.Leave += new System.EventHandler(this.LoginField_Leave);
+            // 
+            // PassField
+            // 
+            this.PassField.Location = new System.Drawing.Point(12, 342);
+            this.PassField.Name = "PassField";
+            this.PassField.Size = new System.Drawing.Size(277, 23);
+            this.PassField.TabIndex = 3;
+            this.PassField.Enter += new System.EventHandler(this.PassField_Enter);
+            this.PassField.Leave += new System.EventHandler(this.PassField_Leave);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(22, 543);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(88, 543);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox4.Location = new System.Drawing.Point(154, 543);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox5.BackgroundImage")));
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox5.Location = new System.Drawing.Point(220, 543);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox5.TabIndex = 7;
+            this.pictureBox5.TabStop = false;
+            // 
+            // RegistrationLabel
+            // 
+            this.RegistrationLabel.AutoSize = true;
+            this.RegistrationLabel.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RegistrationLabel.ForeColor = System.Drawing.Color.White;
+            this.RegistrationLabel.Location = new System.Drawing.Point(8, 391);
+            this.RegistrationLabel.Name = "RegistrationLabel";
+            this.RegistrationLabel.Size = new System.Drawing.Size(127, 24);
+            this.RegistrationLabel.TabIndex = 8;
+            this.RegistrationLabel.Text = "Регистрация";
+            // 
+            // LostPassLabel
+            // 
+            this.LostPassLabel.AutoSize = true;
+            this.LostPassLabel.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LostPassLabel.ForeColor = System.Drawing.Color.White;
+            this.LostPassLabel.Location = new System.Drawing.Point(8, 424);
+            this.LostPassLabel.Name = "LostPassLabel";
+            this.LostPassLabel.Size = new System.Drawing.Size(163, 24);
+            this.LostPassLabel.TabIndex = 9;
+            this.LostPassLabel.Text = "Забыли пароль?";
+            // 
+            // FAQLabel
+            // 
+            this.FAQLabel.AutoSize = true;
+            this.FAQLabel.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FAQLabel.ForeColor = System.Drawing.Color.White;
+            this.FAQLabel.Location = new System.Drawing.Point(8, 457);
+            this.FAQLabel.Name = "FAQLabel";
+            this.FAQLabel.Size = new System.Drawing.Size(261, 24);
+            this.FAQLabel.TabIndex = 10;
+            this.FAQLabel.Text = "Есть вопрос? Нажми сюда!";
+            // 
+            // LoginForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.Sienna;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(984, 621);
+            this.Controls.Add(this.FAQLabel);
+            this.Controls.Add(this.LostPassLabel);
+            this.Controls.Add(this.RegistrationLabel);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.PassField);
+            this.Controls.Add(this.LoginField);
+            this.Controls.Add(this.LogoImage);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoginForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "• Tavern Between Worlds •";
+            ((System.ComponentModel.ISupportInitialize)(this.LogoImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox LogoImage;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox LoginField;
+        private System.Windows.Forms.TextBox PassField;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label RegistrationLabel;
+        private System.Windows.Forms.Label LostPassLabel;
+        private System.Windows.Forms.Label FAQLabel;
     }
 }
 
--- LoginForm.cs
+++ LoginForm.cs
@@ -15,6 +15,69 @@ namespace LaunhcerForMinecraft
         public LoginForm()
         {
             InitializeComponent();
+            LoginField.Text = "Введите логин";
+            LoginField.ForeColor = Color.Gray;
+
+            PassField.Text = "Введите пароль";
+            PassField.ForeColor = Color.Gray;
+
+            
         }
+
+        private void LoginField_Enter(object sender, EventArgs e)
+        {
+            if (LoginField.Text == "Введите логин")
+            {
+                LoginField.Text = "";
+                LoginField.ForeColor = Color.Gray;
+            }
+            else if (LoginField.Text != "Введите логин")
+            {
+                LoginField.ForeColor = Color.Black;
+            }
+              
+        }
+
+        private void LoginField_Leave(object sender, EventArgs e)
+        {
+            if (LoginField.Text == "")
+            {
+                LoginField.Text = "Введите логин";
+                LoginField.ForeColor = Color.Gray;
+            }
+            else if (LoginField.Text != "")
+            {
+                LoginField.ForeColor = Color.Black;
+            }
+        }
+
+        private void PassField_Enter(object sender, EventArgs e)
+        {
+            if (PassField.Text == "Введите пароль")
+            {
+                PassField.ForeColor = Color.Gray;
+                PassField.Text = "";
+            }
+            else if (PassField.Text != "Введите логин")
+            {
+                PassField.ForeColor = Color.Black;
+            }
+
+        }
+
+        private void PassField_Leave(object sender, EventArgs e)
+        {
+            if (PassField.Text == "")
+            {
+                PassField.Text = "Введите пароль";
+                PassField.ForeColor = Color.Gray;
+            }
+            else if (PassField.Text != "")
+            {
+                PassField.ForeColor = Color.Black;
+            }
+        }
+
+        
     }
 }
